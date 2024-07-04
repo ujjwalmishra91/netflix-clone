04a20ffa6ca810564d343b0bbb2379abefec064a
Ujjwal Mishra <94533741+ujjwalmishra91@users.noreply.github.com>
2024-07-04 08:40:34 +0530

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,9 @@
+// And here is the API
+let api_key = "9cc196439951829cb5804d9283ead00f";
+
+// The data coming from the API
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
